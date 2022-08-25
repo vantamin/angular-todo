1cf3845ac6f23db6503b7fa5223cd8fcee453b47
vantamin <vantablack.min@gmail.com>
2022-08-25 15:55:25 +0900

    create: todo module

---
+++ src/app/todo/todo.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TodosComponent } from './todos/todos.component';
+
+@NgModule({
+  declarations: [TodosComponent],
+  imports: [CommonModule],
+  exports: [TodosComponent],
+})
+export class TodoModule {}
---
+++ src/app/todo/todos/todos.component.ts
@@ -0,0 +1,13 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.scss'],
+})
+export class TodosComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}

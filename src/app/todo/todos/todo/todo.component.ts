4d9863cae049d0083af369f8311aadfa5de271fd
vantamin <vantablack.min@gmail.com>
2022-08-26 14:40:58 +0900

    create: todo component

---
+++ src/app/todo/todos/todo/todo.component.ts
@@ -0,0 +1,13 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todo',
+  template: `<input type="checkbox" /> 운동하기`,
+  styles: [],
+})
+export class TodoComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}

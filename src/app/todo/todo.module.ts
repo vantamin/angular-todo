6242fed11c5076cb723379b624d34ec9f6985066
vantamin <vantablack.min@gmail.com>
2022-08-26 15:27:26 +0900

    update: todos template

--- src/app/todo/todo.module.ts
+++ src/app/todo/todo.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { TodosComponent } from './todos/todos.component';
 import { TodoComponent } from './todos/todo/todo.component';
 
 @NgModule({
   declarations: [TodosComponent, TodoComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   exports: [TodosComponent],
 })
 export class TodoModule {}
--- src/app/todo/todos/todos.component.ts
+++ src/app/todo/todos/todos.component.ts
@@ -7,7 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todos.component.scss'],
 })
 export class TodosComponent implements OnInit {
-  constructor() {}
+  newText = '';
+  todos: {
+    done: boolean;
+    text: string;
+  }[] = [];
+
+  constructor() {
+    this.todos = [
+      { done: false, text: '운동하기' },
+      { done: true, text: '공부하기' },
+    ];
+  }
 
   ngOnInit(): void {}
+
+  toggleTodo(todo: { done: boolean; text: string }) {
+    todo.done = !todo.done;
+  }
+
+  addTodo(newText: string) {
+    this.todos.push({
+      done: false,
+      text: newText,
+    });
+    this.newText = '';
+  }
 }
